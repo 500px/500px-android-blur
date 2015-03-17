fc3590ed48b278b72d66b1a44fc36340a2f6d74d
Jun Luo <jun@500px.com>
2015-03-16 23:05:04 -0400

    mildly document the code

--- app/src/main/java/com/fivehundredpx/blurdemo/MainActivity.java
+++ app/src/main/java/com/fivehundredpx/blurdemo/MainActivity.java
@@ -1,10 +1,24 @@
+/*
+ * Copyright (C) 2015 500px Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.fivehundredpx.blurdemo;
 
 import android.animation.ValueAnimator;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -12,6 +26,9 @@ import com.fivehundredpx.android.blur.BlurringView;
 
 import java.util.Random;
 
+/**
+ * Demonstrates the use of the blurring view.
+ */
 public class MainActivity extends ActionBarActivity {
 
     @Override
@@ -20,6 +37,8 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         mBlurringView = (BlurringView) findViewById(R.id.blurring_view);
         View blurredView = findViewById(R.id.blurred_view);
+
+        // Give the blurring view a reference to the blurred view.
         mBlurringView.setBlurredView(blurredView);
 
         mImageViews[0] = (ImageView) findViewById(R.id.image0);
@@ -33,44 +52,28 @@ public class MainActivity extends ActionBarActivity {
         mImageViews[8] = (ImageView) findViewById(R.id.image8);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void shuffle(View view) {
+
+        // Randomly pick a different start in the array of available images.
         int newStartIndex;
         do {
             newStartIndex = mImageIds[mRandom.nextInt(mImageIds.length)];
         } while (newStartIndex == mStartIndex);
         mStartIndex = newStartIndex;
 
+        // Update the images for the image views contained in the blurred view.
         for (int i = 0; i < mImageViews.length; i++) {
             int drawableId = mImageIds[(mStartIndex + i) % mImageIds.length];
             mImageViews[i].setImageDrawable(getResources().getDrawable(drawableId));
         }
+
+        // Invalidates the blurring view when the content of the blurred view changes.
         mBlurringView.invalidate();
     }
 
     private ValueAnimator.AnimatorUpdateListener listener = new ValueAnimator.AnimatorUpdateListener() {
         public void onAnimationUpdate(ValueAnimator animation) {
+            // Invalidates the blurring view for animation updates.
             mBlurringView.invalidate();
         }
     };
--- blurringview/src/main/java/com/fivehundredpx/android/blur/BlurringView.java
+++ blurringview/src/main/java/com/fivehundredpx/android/blur/BlurringView.java
@@ -1,8 +1,20 @@
-package com.fivehundredpx.android.blur;
-
-/**
- * Created by jun on 15-03-12.
+/*
+ * Copyright (C) 2015 500px Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
+package com.fivehundredpx.android.blur;
 
 import android.content.Context;
 import android.content.res.Resources;
@@ -20,23 +32,12 @@ import android.util.AttributeSet;
 import android.view.View;
 
 /**
- * Created by jun on 15-02-20.
+ * A custom view for presenting a dynamically blurred version of another view's content.
+ * <p>
+ * Use {@link #setBlurredView(android.view.View)} to set up the reference to the view to be blurred.
+ * After that, call {@link #invalidate()} to trigger blurring whenever necessary.
  */
 public class BlurringView extends View {
-
-    private int mBlurRadius;
-    private int mDownsampleFactor;
-    private int mOverlayColor;
-
-    private View mBlurredView;
-    private int mBlurredViewWidth, mBlurredViewHeight;
-
-    private boolean mDownsampleFactorChanged;
-    private Bitmap mBitmapToBlur, mBlurredBitmap;
-    private Canvas mBlurringCanvas;
-    private RenderScript mRenderScript;
-    private ScriptIntrinsicBlur mBlurScript;
-    private Allocation mBlurInput, mBlurOutput;
 
     public BlurringView(Context context) {
         this(context, null);
@@ -54,9 +55,14 @@ public class BlurringView extends View {
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.PxBlurringView);
         setBlurRadius(a.getInt(R.styleable.PxBlurringView_blurRadius, defaultBlurRadius));
-        setDownsampleFactor(a.getInt(R.styleable.PxBlurringView_downsampleFactor, defaultDownsampleFactor));
+        setDownsampleFactor(a.getInt(R.styleable.PxBlurringView_downsampleFactor,
+                defaultDownsampleFactor));
         setOverlayColor(a.getColor(R.styleable.PxBlurringView_overlayColor, defaultOverlayColor));
         a.recycle();
+    }
+
+    public void setBlurredView(View blurredView) {
+        mBlurredView = blurredView;
     }
 
     @Override
@@ -64,7 +70,10 @@ public class BlurringView extends View {
         super.onDraw(canvas);
         if (mBlurredView != null) {
             if (prepare()) {
+                // Clears the blurring canvas.
                 mBlurringCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+
+                //
                 mBlurredView.draw(mBlurringCanvas);
                 blur();
 
@@ -80,7 +89,6 @@ public class BlurringView extends View {
 
     public void setBlurRadius(int radius) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-            mBlurRadius = radius;
             mBlurScript.setRadius(radius);
         }
     }
@@ -100,10 +108,6 @@ public class BlurringView extends View {
         mOverlayColor = color;
     }
 
-    public void setBlurredView(View blurredView) {
-        mBlurredView = blurredView;
-    }
-
     private void initializeRenderScript(Context context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
             mRenderScript = RenderScript.create(context);
@@ -118,7 +122,8 @@ public class BlurringView extends View {
             final int width = mBlurredView.getWidth();
             final int height = mBlurredView.getHeight();
 
-            if (mBlurringCanvas == null || mDownsampleFactorChanged || mBlurredViewWidth != width || mBlurredViewHeight != height) {
+            if (mBlurringCanvas == null || mDownsampleFactorChanged
+                    || mBlurredViewWidth != width || mBlurredViewHeight != height) {
                 mDownsampleFactorChanged = false;
 
                 mBlurredViewWidth = width;
@@ -131,13 +136,17 @@ public class BlurringView extends View {
                 scaledWidth = scaledWidth - scaledWidth % 4 + 4;
                 scaledHeight = scaledHeight - scaledHeight % 4 + 4;
 
-                if (mBlurredBitmap == null || mBlurredBitmap.getWidth() != scaledWidth || mBlurredBitmap.getHeight() != scaledHeight) {
-                    mBitmapToBlur = Bitmap.createBitmap(scaledWidth, scaledHeight, Bitmap.Config.ARGB_8888);
+                if (mBlurredBitmap == null
+                        || mBlurredBitmap.getWidth() != scaledWidth
+                        || mBlurredBitmap.getHeight() != scaledHeight) {
+                    mBitmapToBlur = Bitmap.createBitmap(scaledWidth, scaledHeight,
+                            Bitmap.Config.ARGB_8888);
                     if (mBitmapToBlur == null) {
                         return false;
                     }
 
-                    mBlurredBitmap = Bitmap.createBitmap(scaledWidth, scaledHeight, Bitmap.Config.ARGB_8888);
+                    mBlurredBitmap = Bitmap.createBitmap(scaledWidth, scaledHeight,
+                            Bitmap.Config.ARGB_8888);
                     if (mBlurredBitmap == null) {
                         return false;
                     }
@@ -154,7 +163,7 @@ public class BlurringView extends View {
         }
     }
 
-    public void blur() {
+    private void blur() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
             mBlurInput.copyFrom(mBitmapToBlur);
             mBlurScript.setInput(mBlurInput);
@@ -162,4 +171,18 @@ public class BlurringView extends View {
             mBlurOutput.copyTo(mBlurredBitmap);
         }
     }
+
+    private int mDownsampleFactor;
+    private int mOverlayColor;
+
+    private View mBlurredView;
+    private int mBlurredViewWidth, mBlurredViewHeight;
+
+    private boolean mDownsampleFactorChanged;
+    private Bitmap mBitmapToBlur, mBlurredBitmap;
+    private Canvas mBlurringCanvas;
+    private RenderScript mRenderScript;
+    private ScriptIntrinsicBlur mBlurScript;
+    private Allocation mBlurInput, mBlurOutput;
+
 }
